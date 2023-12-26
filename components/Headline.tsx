0060c1b05dcc59cbe75d8620996e55a4bd1b5d9c
aktio <himenoakito1019@gmail.com>
2023-12-27 08:14:19 +0900

    ♻️ Main部分のコンポーネント化

--- components/Headline.tsx
+++ components/Headline.tsx
@@ -1,16 +1,24 @@
-import React from 'react'
-import styles from '../app/page.module.css'
+import React from "react";
+import styles from "../app/page.module.css";
 
-export function Headline(props: { title?: string, page?: string }) {
-  console.log(props.title);
+export function Headline(props: {
+  children?: React.ReactNode;
+  title?: string;
+  page?: string;
+  number?: number;
+  array?: (string | number)[];
+  obj?: { foo: string; bar: string };
+  boolean?: boolean;
+  comp?: React.ReactNode;
+  onClick?: () => void;
+}) {
   return (
     <div>
       <h1 className={styles.title}>{props.page} Page</h1>
 
       <p className={styles.description}>
-        Get Started by editiong{' '}
-        <code className={styles.code}>app/{props.page}.tsx</code>
+        Get Started by editing {props.children}
       </p>
     </div>
-  )
+  );
 }
---
+++ components/Main.tsx
@@ -0,0 +1,107 @@
+import Image from "next/image";
+import styles from "@/app/page.module.css";
+import { Headline } from "@/components/Headline";
+
+export function Main(props: { page?: string }) {
+  return (
+    <div>
+      <main className={styles.main}>
+        <div className={styles.description}>
+          <Headline
+            page={props.page}
+            number={1111}
+            array={[1, 2, 3]}
+            obj={{ foo: "foo", bar: "bar" }}
+            // boolean={true}
+            // 真偽値の場合以下のように省略できる
+            boolean
+          >
+            comp={<code className={styles.code}>app/{props.page}.tsx</code>}
+          </Headline>
+          <div>
+            <a
+              href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              By{" "}
+              <Image
+                src="/vercel.svg"
+                alt="Vercel Logo"
+                className={styles.vercelLogo}
+                width={100}
+                height={24}
+                priority
+              />
+            </a>
+          </div>
+        </div>
+
+        <div className={styles.center}>
+          <Image
+            className={styles.logo}
+            src="/next.svg"
+            alt="Next.js Logo"
+            width={180}
+            height={37}
+            priority
+          />
+        </div>
+
+        <div className={styles.grid}>
+          <a
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2>
+              Docs <span>-&gt;</span>
+            </h2>
+            <p>Find in-depth information about Next.js features and API.</p>
+          </a>
+
+          <a
+            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2>
+              Learn <span>-&gt;</span>
+            </h2>
+            <p>
+              Learn about Next.js in an interactive course with&nbsp;quizzes!
+            </p>
+          </a>
+
+          <a
+            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2>
+              Templates <span>-&gt;</span>
+            </h2>
+            <p>Explore starter templates for Next.js.</p>
+          </a>
+
+          <a
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2>
+              Deploy <span>-&gt;</span>
+            </h2>
+            <p>
+              Instantly deploy your Next.js site to a shareable URL with Vercel.
+            </p>
+          </a>
+        </div>
+      </main>
+    </div>
+  );
+}
