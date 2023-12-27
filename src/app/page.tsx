41a50bcb4cca123f1e9cacf0464dc57ec22583a7
aktio <himenoakito1019@gmail.com>
2023-12-27 12:47:50 +0900

    ⚗️ 不要な記述でいつか消しますが、試しにクリックイベントを書いてみました。

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
+"use client";
 import { Header } from "@/src/components/Header";
 import { Main } from "@/src/components/Main";
 import { Footer } from "@/src/components/Footer";
+import { useCallback } from "react";
 
 export default function Home() {
+  const handleClick = useCallback((e: any) => {
+    e.preventDefault();
+    alert(123);
+  }, []);
+
   return (
     <div>
       <Header />
-
+      <a href="/about" onClick={handleClick}>
+        ボタン
+      </a>
       <Main page="index" />
 
       <Footer />
