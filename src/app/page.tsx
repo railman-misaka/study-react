ec36230cc3364e721523b46023e04c837a923d0c
aktio <himenoakito1019@gmail.com>
2023-12-27 13:03:34 +0900

    ✨ 講座用のuseEffectを導入

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,22 @@
 import { Header } from "@/src/components/Header";
 import { Main } from "@/src/components/Main";
 import { Footer } from "@/src/components/Footer";
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 
 export default function Home() {
   const handleClick = useCallback((e: any) => {
     e.preventDefault();
     alert(123);
+  }, []);
+
+  useEffect(() => {
+    //ここからマウント処理
+    document.body.style.backgroundColor = "lightblue";
+
+    //ここからアンマウント処理
+    return () => {
+      document.body.style.backgroundColor = "";
+    };
   }, []);
 
   return (
--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Image from "next/image";
 import styles from "@/src/app/page.module.css";
 import { Headline } from "@/src/components/Headline";
